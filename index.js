c111c91eb600f9794cd8340661d0463d802513c3
Wes Bourke <wesbourkeavl@gmail.com>
2022-02-23 11:38:43 -0500

    commit tree 76bf2580daa86a27381a48f4255c011fa309ad4f

--- index.js
+++ index.js
@@ -1,1 +1,49 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(blocks) {
+    if (blocks >= 42) {
+        return blocks - 42;
+    }
+    else {
+        return 42 - blocks;
+    }
+}
+
+function distanceFromHqInFeet(blocks) {
+    if (blocks >= 42) {
+        let b = blocks - 42;
+        return b * 264;
+    }
+    else {
+        let b = 42 - blocks;
+        return b * 264
+    }
+}
+
+function distanceTravelledInFeet(start, destination) {
+    if (destination > start) {
+        return (destination - start) * 264;
+    }
+    else if (start > destination) {
+        return (start - destination) * 264;
+    }
+}
+
+function calculatesFarePrice(start, destination) {
+    let distance = Math.abs(distanceTravelledInFeet(start, destination));
+    console.log("==========================================================")
+    console.log(distance);
+
+    if (distance < 400) {
+        return 0;
+    }
+    else if (distance > 400 && distance <= 2000) {
+        return (distance - 400) * .02;
+    }
+    else if (distance > 2000 && distance <= 2500) {
+        return 25;
+    }
+    else if (distance > 2500) {
+        return 'cannot travel that far'
+    }
+
+}
